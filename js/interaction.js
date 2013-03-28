643fdf73da7354f5865f0e1a09f0865091571147
kenjisaito <k.saito.1985@gmail.com>
2013-03-28 18:08:56 +0100

    update interaction.js

--- js/interaction.js
+++ js/interaction.js
@@ -6,6 +6,11 @@
  * To change this template use File | Settings | File Templates.
  */
 var mobileStatus;
+var touchDownStatus, mouseDownStatus;
+var touchPosition, touchVelocity;
+var mousePosition, mouseVelocity;
+
+var agent = navigator.userAgent;
 
 if(agent.search(/iPhone/) != -1 || agent.search(/iPad/) != -1 || agent.search(/iPod/) != -1 || agent.search(/Android/) != -1){
     mobileStatus = true;
@@ -22,4 +27,68 @@ if(mobileStatus){
     canvas.addEventListener("mousedown", mouse_down);
     canvas.addEventListener("mousemove", mouse_move);
     canvas.addEventListener("mouseup", mouse_up);
+}
+
+function mouse_down(e){
+
+    mouseDownStatus = true;
+    mousePosition = {x: e.x, y: e.y};
+    mouseVelocity = {x: 0, y: 0};
+}
+
+//    ------------------
+
+function mouse_move(e){
+    if(mouseDownStatus){
+
+        var posX = e.x;
+        var posY = e.y;
+
+        mouseVelocity = {x: interactPosition.x - posX, y: interactPosition.y - posY};
+        mousePosition = {x: posX, y: posY};
+    }
+}
+
+//    ------------------
+
+function mouse_up(e){
+    mousePosition = {x: e.x, y: e.y};
+    mouseVelocity = {x: 0, y: 0};
+    mouseDownStatus = false;
+}
+
+function touch_start(e){
+
+    var touch = e.touches[0];
+    touchDownStatus = true;
+    touchPosition = {x: touch.pageX, y: touch.pageY};
+    touchVelocity = {x: 0, y: 0};
+
+    e.preventDefault();
+}
+
+//    ------------------
+
+function touch_move(e){
+    if(touchDownStatus){
+        var touch = e.touches[0];
+        var posX = touch.pageX;
+        var posY = touch.pageY;
+
+        touchVelocity = {x: interactPosition.x - posX, y: interactPosition.y - posY};
+        touchPosition = {x: posX, y: posY};
+
+    }
+    e.preventDefault();
+
+}
+
+//    ------------------
+
+function touch_end(e){
+
+    touchDownStatus = false;
+
+    touchVelocity = {x: 0, y: 0};
+    e.preventDefault();
 }
